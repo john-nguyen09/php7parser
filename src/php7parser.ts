354781346ea204a08de73090deb255d1aaca434f
Ben Mewburn <ben@mewburn.id.au>
2017-01-29 14:25:25 +1100

    refactor

--- src/php7parser.ts
+++ src/php7parser.ts
@@ -1,8 +1,8 @@
-/* Copyright © Ben Mewburn ben@mewburn.id.au
+/* Copyright (c) Ben Mewburn ben@mewburn.id.au
  * Licensed under the MIT Licence.
  */
 
 'use strict';
 
-export {Lexer, LexerMode, Token, TokenType, Position, Range} from './lexer';
-export {Parser, PhraseType, PhraseFlag, NodeFactory, Phrase} from './parser';
+export { Lexer, LexerMode, Token, TokenType } from './lexer';
+export { Parser, PhraseType, Phrase } from './parser';
