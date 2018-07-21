925168ea4c8d7d78e0e31cc951be50eaaf39bf51
Ben Mewburn <ben@mewburn.id.au>
2018-07-21 17:20:28 +1000

    typeguard defintions

--- src/php7parser.ts
+++ src/php7parser.ts
@@ -4,6 +4,17 @@
 
 'use strict';
 
+import { Token, TokenKind } from './lexer';
+import { PhraseKind, Phrase } from './phrase';
+
 export { Lexer, LexerMode, Token, TokenKind, tokenKindToString } from './lexer';
 export { Parser } from './parser';
 export * from './phrase';
+
+export function isToken(node:{kind:number}): node is Token {
+    return node && node.kind !== undefined && node.kind <= TokenKind.Whitespace;
+}
+
+export function isPhrase(node:{kind:number}): node is Phrase {
+    return node && node.kind >= PhraseKind.Unknown;
+}
